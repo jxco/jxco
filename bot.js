5e6ff0027d121779fff1cb48f191c7e6e9baad57
jxco <43864779+jxco@users.noreply.github.com>
2018-11-02 21:40:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,41 @@
 var Discord = require("discord.js");
 var client = new Discord.Client();
 
-client.on("message", message => {
+client.on('message', message => {
     var prefix = "!";
+    
         if (message.author.id === client.user.id) return;
         if (message.guild) {
        let embed = new Discord.RichEmbed()
         let args = message.content.split(' ').slice(1).join(' ');
     if(message.content.split(' ')[0] == prefix + 'tkk') {
         if (!args[1]) {
-    message.channel.send("**bc <message>**");
+    message.channel.send("**!tkk <message>**");
     return;
     }
             message.guild.members.forEach(m => {
        if(!message.member.hasPermission('ADMINISTRATOR')) return;
-                m.send(args);
+                var bc = new Discord.RichEmbed()
+                .addField('» السيرفر :', `${message.guild.name}`)
+                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+                .addField(' » الرسالة : ', args)
+                .setColor('#ff0000')
+                // m.send(`[${m}]`);
+                m.send(`${m}`,{embed: bc});
             });
-            const AziRo = new Discord.RichEmbed()
-            .setAuthor(message.author.username, message.author.avatarURL)   
-            .setTitle('✅| جاري ارسال رسالتك ') 
-            .addBlankField(true)
-            .addField('♨| عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
-            .addField('📝| الرسالة ', args)
-            .setColor('RANDOM')  
-            message.channel.sendEmbed(AziRo);          
+                const AziRo = new Discord.RichEmbed()
+        .setAuthor(message.author.username, message.author.avatarURL)   
+        .setTitle('✔️ | جاري ارسال رسالتك ') 
+        .addBlankField(true)
+        .addField('👥 | عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
+        .addField('📋| الرسالة ', args)
+        .setColor('RANDOM')  
+        message.channel.sendEmbed(AziRo);          
         }
+         
         } else {
             return;
         }
     });
+
 client.login(process.env.BOT_TOKEN); 
