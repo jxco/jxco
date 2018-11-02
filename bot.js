4ff1aa6fef501d993d3841bd507013160fe76948
jxco <43864779+jxco@users.noreply.github.com>
2018-11-02 21:23:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,5 @@
 var Discord = require("discord.js");
 var client = new Discord.Client();
-var prefix = "-" // PREFIX 
-client.on("message", (message) => {
-    if(message.content.startsWith(prefix+"email")) {
-        message.channel.send(JSON.stringify({
-            email: Math.random().toString(36).slice(4).trim()+"@gmail.com",
-            password: Math.random().toString(36).slice(4).trim()
-        }))
-    }
-})
 
 client.on("message", message => {
     var prefix = "!";
@@ -16,7 +7,7 @@ client.on("message", message => {
         if (message.guild) {
        let embed = new Discord.RichEmbed()
         let args = message.content.split(' ').slice(1).join(' ');
-    if(message.content.split(' ')[0] == prefix + 'bc') {
+    if(message.content.split(' ')[0] == prefix + 'tkk') {
         if (!args[1]) {
     message.channel.send("**bc <message>**");
     return;
@@ -38,70 +29,4 @@ client.on("message", message => {
             return;
         }
     });
-
-
-client.on('message', message => {
-    var prefix = "f!";
-    
-        if (message.author.id === client.user.id) return;
-        if (message.guild) {
-       let embed = new Discord.RichEmbed()
-        let args = message.content.split(' ').slice(1).join(' ');
-    if(message.content.split(' ')[0] == prefix + 'bc') {
-        if (!args[1]) {
-    message.channel.send("**f!bc <message>**");
-    return;
-    }
-            message.guild.members.forEach(m => {
-       if(!message.member.hasPermission('ADMINISTRATOR')) return;
-                var bc = new Discord.RichEmbed()
-                .addField('» السيرفر :', `${message.guild.name}`)
-                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
-                .addField(' » الرسالة : ', args)
-                .setColor('#ff0000')
-                // m.send(`[${m}]`);
-                m.send(`${m}`,{embed: bc});
-            });
-        }
-        } else {
-            return;
-        }
-    });
-    
-
-client.on('message', message => {
-var prefix = ".";
-    if (message.author.id === client.user.id) return;
-    if (message.guild) {
-   let embed = new Discord.RichEmbed()
-    let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'bc') {
-    if (!args[1]) {
-message.channel.send("**.bc <message>**");
-return;
-}
-        message.guild.members.forEach(m => {
-   if(!message.member.hasPermission('ADMINISTRATOR')) return;
-            var bc = new Discord.RichEmbed()
-            .setAuthor(message.author.username, message.author.avatarURL)
-            .addField('** الـسيرفر**', `${message.guild.name}`,true)
-            .addField(' **الـمرسل **', `${message.author.username}#${message.author.discriminator}`,true)
-            .addField(' **الرسالة** ', args)
-            .setThumbnail(message.guild.iconURL)
-            .setColor('RANDOM')
-            m.send(`${m}`,{embed: bc});
-        });
-        const AziRo = new Discord.RichEmbed()
-        .setAuthor(message.author.username, message.author.avatarURL)   
-        .setTitle('✔️ | جاري ارسال رسالتك ') 
-        .addBlankField(true)
-        .addField('👥 | عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
-        .addField('📋| الرسالة ', args)
-        .setColor('RANDOM')  
-        message.channel.sendEmbed(AziRo);          
-    }
-    } else {
-        return;
-    }
-});
 client.login(process.env.BOT_TOKEN); 
